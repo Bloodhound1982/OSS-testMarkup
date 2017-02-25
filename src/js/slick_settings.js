50c5f95bba8ff2eebeafeabb3c3f9f88157fb50f
Alex <bloodhound1982@gmail.com>
2017-02-25 15:26:01 +0200

    complete carousel

---
+++ src/js/slick_settings.js
@@ -0,0 +1,10 @@
+$('.carousel_slick').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
