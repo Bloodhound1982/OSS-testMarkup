db2e5a14c7202a02e7aef4b77b83a097b47d55c9
Alex <bloodhound1982@gmail.com>
2017-02-26 11:59:47 +0200

    complete markup on big screens

---
+++ dist/js/slick_settings.js
@@ -0,0 +1,12 @@
+$('.carousel_slick').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzbGlja19zZXR0aW5ncy5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIkKCcuY2Fyb3VzZWxfc2xpY2snKS5zbGljayh7XG4gICAgZG90czogZmFsc2UsXG4gICAgYXJyb3dzOiBmYWxzZSxcbiAgICBpbmZpbml0ZTogdHJ1ZSxcbiAgICBzcGVlZDogMzAwLFxuICAgIHNsaWRlc1RvU2hvdzogMSxcbiAgICBhZGFwdGl2ZUhlaWdodDogdHJ1ZSxcbiAgICBhdXRvcGxheTogdHJ1ZSxcbiAgICBhdXRvcGxheVNwZWVkOiAyMDAwXG59KTtcbiJdLCJmaWxlIjoic2xpY2tfc2V0dGluZ3MuanMifQ==
